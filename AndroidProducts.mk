8a94e15ef22b45d3fd5b48e568d06dd46c638c23
alyoshakaramazov966 <56597555+alyoshakaramazov966@users.noreply.github.com>
2021-04-23 17:36:33 -0500

    Update AndroidProducts.mk
    add config files maruos

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 The Android Open Source Project
+# Copyright 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,4 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_walleye.mk \
-    $(LOCAL_DIR)/aosp_walleye_test.mk \
-
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/walleye/lineage_walleye.mk
+    $(LOCAL_DIR)/maru_walleye.mk \
